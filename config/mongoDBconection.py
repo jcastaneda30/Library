a493ea61c4629f09f6e6a6ba203d17830e3b1a7c
Jaider Castañeda Villa <jaidercasta30@gmail.com>
2023-06-29 10:05:01 -0500

    inicio base de datos y aplicacion principal

---
+++ config/mongoDBconection.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+def get_client():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb+srv://jcasta30:ba4Rwk8zxJLgCpVO@library.ztmxmow.mongodb.net/?retryWrites=true&w=majority"
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['Library']
+
